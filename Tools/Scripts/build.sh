09836d73e1830d0e85906f4b53d7c6b0d8dc4bcd
li1553770945 <1553770945@qq.com>
2022-05-07 15:14:37 +0800

    调整项目目录结构

---
+++ Tools/Scripts/build.sh
@@ -0,0 +1,11 @@
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Boot/main.cpp                  -o ../../Build/main.o        -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Boot/Start.S                   -o ../../Build/Start.o       -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Boot/SystemInfo.cpp            -o ../../Build/SystemInfo.o  -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Trap/Clock.cpp                 -o ../../Build/Clock.o 	  -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Trap/Interrupt.cpp             -o ../../Build/Interrupt.o   -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Trap/Trap.cpp                  -o ../../Build/Trap.o        -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Trap/TrapEntry.S               -o ../../Build/TrapEntry.o   -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Library/Kout.cpp               -o ../../Build/Kout.o        -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Library/String/Convert.cpp     -o ../../Build/Convert.o     -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-g++ -nostdlib -c ../../Kernel/Library/String/StringTools.cpp -o ../../Build/StringTools.o -I"../../Include" -mcmodel=medany 
+riscv64-unknown-elf-ld -o ../../Build/1.elf -T ../../Linker/link.ld ../../Build/*.o
---
+++ Tools/Scripts/run.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv64 -machine virt -kernel ../../Build/1.elf -m 8M -nographic -bios ../../SBI/rustsbi-qemu	
