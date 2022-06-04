c2ffb3a35e5edb90e057132d1758f644f94f3033
li1553770945 <1553770945@qq.com>
2022-06-04 18:12:21 +0800

    Debug

--- Include/File/FAT32.hpp
+++ Include/File/FAT32.hpp
@@ -25,36 +25,36 @@ class FAT32Device :public StorageDevice {
 public:
 	ErrorType Init()
 	{
-		using namespace POS;
-		kout[Debug]<<"FAT32Device init"<<endl;
-		disk=new unsigned char[size];
-		if (disk==nullptr)
-			kout[Fault]<<"Failed to new disk buffer!"<<endl;
-		else kout[Info]<<"Allocate disk buffer OK, "<<disk<<endl;
-		for (int lba=0;lba<size/512;++lba)
-			kout<<"ReadLBA "<<lba<<endl,
-			sdcard_read_sector((Sector*)disk+lba+shift/512,lba);
-		kout[Info]<<"Init disk buffer OK"<<endl;
+//		using namespace POS;
+//		kout[Debug]<<"FAT32Device init"<<endl;
+//		disk=new unsigned char[size];
+//		if (disk==nullptr)
+//			kout[Fault]<<"Failed to new disk buffer!"<<endl;
+//		else kout[Info]<<"Allocate disk buffer OK, "<<disk<<endl;
+//		for (int lba=0;lba<size/512;++lba)
+//			kout<<"ReadLBA "<<lba+shift/512<<endl,
+//			sdcard_read_sector((Sector*)disk+lba+shift/512,lba);
+//		kout[Info]<<"Init disk buffer OK"<<endl;
 		return ERR_None;;
 	}
 	ErrorType Read(Uint64 lba, unsigned char* buffer)
 	{
-//		sdcard_read_sector((Sector*)buffer,lba);
+		sdcard_read_sector((Sector*)buffer,lba);
 		
-		using namespace POS;
-		if (lba>=size/512+shift/512)
-			sdcard_read_sector((Sector*)buffer,lba);
-		POS::MemcpyT<unsigned char>(buffer,disk+lba*512,512);
+//		using namespace POS;
+//		if (lba>=size/512+shift/512)
+//			sdcard_read_sector((Sector*)buffer,lba);
+//		POS::MemcpyT<unsigned char>(buffer,disk+lba*512,512);
 		return ERR_None;
 	}
 	ErrorType Write(Uint64 lba, unsigned char* buffer)
 	{
-//		sdcard_write_sector((Sector*)buffer,lba);
+		sdcard_write_sector((Sector*)buffer,lba);
 		
-		using namespace POS;
-		if (lba>=size/512+shift/512)
-			sdcard_write_sector((Sector*)buffer,lba);
-		POS::MemcpyT<unsigned char>(disk+lba*512,buffer,512);
+//		using namespace POS;
+//		if (lba>=size/512+shift/512)
+//			sdcard_write_sector((Sector*)buffer,lba);
+//		POS::MemcpyT<unsigned char>(disk+lba*512,buffer,512);
 		return ERR_None;
 	}
 };
